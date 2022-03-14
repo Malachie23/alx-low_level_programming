23917d368752157ad0490073478b74d0a545183f
malachie23 <malachie433@gmail.com>
2022-03-14 15:56:08 -0700

    positive negative

---
+++ 0x01-variables_if_else_while/0-pisitive_negative.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <time.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - POSITIVE NEGATIVE
+ * Return: 0
+ */#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+		int n;
+
+			srand(time(0));
+				n = rand() - RAND_MAX / 2;
+					/* your code goes there */
+					return (0);
+}#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - POSITIVE NEGATIVE
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+
+	/* your code goes there */
+	if (n > 0) 
+	{
+		printf("is positive");
+	} else if (n = 0 )
+	{
+		printf("is zero");
+	}else 
+	{
+	printf("is negative");
+	}
+	
+	return (0);
+}
