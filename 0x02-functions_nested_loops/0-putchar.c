8a6992bb7adbe6d483b7cbcc6b42adf2e23cc124
malachie23 <malachie433@gmail.com>
2022-03-15 15:56:22 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - ENRTY
+ * Return: 0 on success
+ */
+
+int main()
+{
+	char p = '_putschar';
+
+	putchar(p);
+
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+functiona nd nested loop
