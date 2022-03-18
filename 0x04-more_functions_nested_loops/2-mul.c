60eeb8134c3f0e91ba9b1ab33054700150e00282
malachie23 <malachie433@gmail.com>
2022-03-17 18:09:12 -0700

    The numbers speaks themselves

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - checks multiplication of two numbers
+ * @a: value one
+ * @b: value two
+ * Return: always 0.
+ */
+int mul(int a, int b)
+{
+	int r = a * b;
+
+	return (r);
+}
