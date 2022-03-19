67ae9d3c6200ce9f9f03238831052be77de478b1
malachie23 <malachie433@gmail.com>
2022-03-18 17:21:24 -0700

    Alupeer an dLower

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - mian function
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char a, b;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	for (b = 'A'; b <= 'Z'; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}
