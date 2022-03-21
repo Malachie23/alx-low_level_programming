55e27fe3460b20d6579e94a0beb7ac2d66267fa2
malachie23 <malachie433@gmail.com>
2022-03-21 14:42:37 -0700

    String length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _strlen - fucntion that count length
+ * @s: Input
+ * Return: Always length number..
+ */
+
+int _strlen(char *s)
+{
+	int n;
+
+	n = strlen(s);
+	return (n);
+}
