797198dd356326ae2e3b95e29440455674b0b41c
malachie23 <malachie433@gmail.com>
2022-03-22 04:32:18 -0700

    reverse \string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * rev_string - reverse
+ * @s: string charcter
+ */
+
+void rev_string(char *s)
+{
+	char r[10];
+	int begin;
+	int end;
+	int count;
+
+	count = 0;
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	end = count - 1;
+	for (begin = 0; begin < count; begin++)
+	{
+		r[begin] = s[end];
+		end--;
+	}
+	r[begin] = '\0';
+	printf("%s\n", r);
+}
