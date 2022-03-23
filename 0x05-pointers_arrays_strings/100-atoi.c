caa9f5ebf4b0b4d5c01277d02bd069cb1a6b2a6f
malachie23 <malachie433@gmail.com>
2022-03-22 18:07:52 -0700

    Convert

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _atoi - convert string into numbers
+ *@s: pparameter
+ * Return: number
+ */
+
+int _atoi(char *s)
+{
+	int v;
+
+	v = atoi(s);
+	return (v);
+}
