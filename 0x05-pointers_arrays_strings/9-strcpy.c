e450a0d221ff78b0f6fbff5bc019070c3aad50a9
malachie23 <malachie433@gmail.com>
2022-03-22 17:53:18 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcpy - Copy
+ * @dest: input char
+ * @src: input char
+ * Return: Des value
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
