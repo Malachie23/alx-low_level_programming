4378b9a494701a4d3d6ad93b7372061478ba6529
malachie23 <malachie433@gmail.com>
2022-03-30 02:24:48 -0700

    locates charact

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strchr - Function that loacates a character
+ * @s: characters
+ * @c: character.
+ * Return: characters.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int a = 0, b;
+
+	while (s[a] !='\0')
+	{
+		a++;
+	}
+	for (b = 0; b <= a; b++)
+	{
+		if (c == s[b])
+		{
+			s += b;
+			return (s);
+		}
+	}
+	return('\0');
+}
